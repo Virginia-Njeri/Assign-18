07d5e6a4753eebe13cb765d2d2a0237e6c02853d
virginia-njeri <you@example.comvirginiaanjeri@gmail.com>
2022-07-23 09:55:05 +0300

    changes

--- app/src/main/java/com/naburi/mycontacts/ContactRVAdapter.kt
+++ app/src/main/java/com/naburi/mycontacts/ContactRVAdapter.kt
@@ -1,10 +1,12 @@
 package com.naburi.mycontacts
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
 import com.naburi.mycontacts.databinding.ContactListItemBinding
@@ -29,10 +31,28 @@ class ContactRVAdapter(var contactList:List<Contact>):
             .centerCrop()
             .placeholder(R.drawable.ic_baseline_person_24)
             .into(holder.binding.ivContact)
+
+        val context=holder.itemView.context
+        holder.binding.ivContact.setOnClickListener{
+            Toast
+                .makeText(context,"You are beautiful",Toast.LENGTH_LONG).show()
+
+        }
+        holder.binding.cvContact.setOnClickListener{
+            val intent=Intent(context,ViewContactActivity::class.java)
+            intent.putExtra("NAME",currentContact.name)
+            intent.putExtra("Email",currentContact.email)
+            intent.putExtra("ADDRESS",currentContact.address)
+            intent.putExtra("PHONE_NUMBER",currentContact.phoneNumber)
+            intent.putExtra("IMAGE",currentContact.image)
+            context.startActivity(intent)
+
+
+        }
     }
     override fun getItemCount(): Int {
         return contactList.size
     }
 
 }
-class ContactViewHolder(val binding: ContactListItemBinding):RecyclerView.ViewHolder(binding.root){}
+class ContactViewHolder(val binding: ContactListItemBinding):RecyclerView.ViewHolder(binding.root)
--- app/src/main/java/com/naburi/mycontacts/ViewContactActivity.kt
+++ app/src/main/java/com/naburi/mycontacts/ViewContactActivity.kt
@@ -2,10 +2,34 @@ package com.naburi.mycontacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.naburi.mycontacts.databinding.ActivityMainBinding
+import com.naburi.mycontacts.databinding.ActivityViewContactBinding
+import com.squareup.picasso.Picasso
 
 class ViewContactActivity : AppCompatActivity() {
+    lateinit var binding: ActivityViewContactBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityViewContactBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_contact)
+        setContentView(binding.root)
+        getExtra()
+    }
+    fun getExtra (){
+        var name=intent.extras?.getString("NAME","")
+        var phoneNumber=intent.extras?.getString("PHONE_NUMBER","")
+        var address=intent.extras?.getString("ADDRESS","")
+        var email=intent.extras?.getString("Email","")
+        var image=binding.imgPerson
+
+        Toast.makeText(this,name,Toast.LENGTH_LONG).show()
+        binding.tvNam.text=name
+        binding.tvNumber.text=phoneNumber
+        binding.tvEmail2.text=email
+        binding.tvAdress.text=address
+        Picasso.get().load(intent.getStringExtra("IMAGE"))
+            .resize(200,200)
+            .centerCrop()
+            .into(image)
     }
 }
